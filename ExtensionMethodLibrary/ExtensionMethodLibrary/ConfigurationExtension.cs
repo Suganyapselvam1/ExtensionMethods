084984a1faa1df88ed5867802b57f85cae36be86
Suganya Panneerselvam <suganya.panneerselvam@dataquest.com.au>
2021-10-29 11:17:06 +0530

    wrote extension method

---
+++ ExtensionMethodLibrary/ExtensionMethodLibrary/ConfigurationExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodLibrary
+{
+    public static class ConfigurationExtension
+    {
+        public static bool IsLoaded(this IConfiguration config)
+        {
+            return config!=null && config.AsEnumerable().Any();
+        }
+    }
+}
---
+++ ExtensionMethodLibrary/ExtensionMethodLibrary/ConfigurationTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodLibrary
+{
+    class ConfigurationTest
+    {
+        [Test]
+        public void IsLoaded()
+        {
+            IConfiguration config = null;
+            Assert.IsFalse(config.IsLoaded());
+        }
+    }
+}
